c1c9a24ea73ff7b9647f5fab3d94eddff5a32312
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-09-25 00:50:19 -0300

    terminamos la calculadora de suma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculadora-Angular';
+    titulo = 'Calculadora Angular';
+    operandoA: number = 0;
+    operandoB: number = 0;
+    resultado: number = 0;
+
+    sumar(): void{
+        this.resultado = this.operandoA + this.operandoB;
+    }
+
 }
